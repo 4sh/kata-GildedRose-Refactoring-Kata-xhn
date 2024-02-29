f4db20b46157ad7ebd949d2e184ca41975133d5b
Xavier Hanin <xavier.hanin@4sh.fr>
2024-02-29 12:00:46 +0100

    refactor extract logic to item types

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,64 +4,84 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         items.forEach { item ->
-            if (!ItemType.SULFURAS.isItem(item)) {
-                when {
-                    ItemType.BACKSTAGE.isItem(item) && item.sellIn < 6 -> {
-                        item.addQuality(3)
-                    }
-
-                    ItemType.BACKSTAGE.isItem(item) && item.sellIn < 11 -> {
-                        item.addQuality(2)
-                    }
-
-                    listOf(ItemType.BACKSTAGE, ItemType.BRIE).any { it.isItem(item) } -> item.addQuality(1)
-                    else -> item.addQuality(-1)
-                }
-                if (ItemType.CONJURED.isItem(item)) {
-                    item.addQuality(-1)
-                }
-
-                item.sellIn -= 1
-
-                if (item.sellIn < 0) {
-                    if (ItemType.BRIE.isItem(item)) {
-                        item.addQuality(1)
-                    } else {
-                        if (!ItemType.BACKSTAGE.isItem(item)) {
-                            item.addQuality(-1)
-                        } else {
-                            item.addQuality(-item.quality)
-                        }
-                    }
-                }
-            }
+            item.itemType?.dayPassedUpdateItem(item)
         }
     }
 
-    private fun Item.addQuality(value: Int) {
-        quality += value
-        quality = quality.coerceIn(0, 50)
-    }
+}
+
+private fun Item.addQuality(value: Int) {
+    quality += value
+    quality = quality.coerceIn(0, 50)
 }
 
 enum class ItemType(val label: String) {
-    BACKSTAGE("Backstage passes to a TAFKAL80ETC concert"),
-    SULFURAS("Sulfuras, Hand of Ragnaros"),
-    BRIE("Aged Brie"),
+    BACKSTAGE("Backstage passes to a TAFKAL80ETC concert") {
+        override fun dayPassedQualityUpdate(item: Item)
+            = when {
+                item.sellIn < 0 -> {
+                    QualityUpdate(-item.quality)
+                }
+                item.sellIn < 5 -> {
+                    QualityUpdate(3)
+                }
+                item.sellIn < 10 -> {
+                    QualityUpdate(2)
+                }
+                else -> QualityUpdate(1)
+            }
+    },
+    SULFURAS("Sulfuras, Hand of Ragnaros") {
+        override fun dayPassedUpdateItem(item: Item) {
+            // never update sulfuras
+        }
+    },
+    BRIE("Aged Brie") {
+        override fun dayPassedQualityUpdate(item: Item): QualityUpdate {
+            return BASIC.dayPassedQualityUpdate(item) * -1
+        }
+    },
     CONJURED("Conjured") {
         override fun isItem(item: Item): Boolean {
             return item.name.startsWith(label)
+        }
+
+        override fun dayPassedQualityUpdate(item: Item): QualityUpdate {
+            return BASIC.dayPassedQualityUpdate(item) * 2
         }
     },
     BASIC("basic") {
         override fun isItem(item: Item): Boolean {
             return true
         }
+
+        override fun dayPassedQualityUpdate(item: Item): QualityUpdate {
+            if (item.sellIn >= 0) {
+                return QualityUpdate(-1)
+            } else {
+                return QualityUpdate(-2)
+            }
+        }
     }
     ;
-    
-    open fun isItem(item:Item): Boolean = item.name == label
+
+    open fun isItem(item: Item): Boolean = item.name == label
+
+    open fun dayPassedUpdateItem(item: Item) {
+        item.dayPassedUpdateSellIn()
+        item.addQuality(dayPassedQualityUpdate(item).qualityInc)
+    }
+    open fun dayPassedQualityUpdate(item: Item): QualityUpdate = QualityUpdate(0)
+}
+
+data class QualityUpdate(val qualityInc: Int) {
+    operator fun times(mul: Int): QualityUpdate {
+        return QualityUpdate(qualityInc * mul)
+    }
 }
 
 val Item.itemType get() = ItemType.entries.firstOrNull { it.isItem(this) }
+
+
+fun Item.dayPassedUpdateSellIn() { sellIn -- }
 
