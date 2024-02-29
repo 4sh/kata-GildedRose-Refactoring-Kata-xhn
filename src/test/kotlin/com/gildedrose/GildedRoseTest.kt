f4113d6746c2dfe0cd94e8e707a77e4ecd643e8d
Xavier Hanin <xavier.hanin@4sh.fr>
2024-02-29 12:13:44 +0100

    add tests for conjured

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,22 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
+    fun `should conjured item decrease quality by 2 before sellin`() {
+        val items = listOf(Item("Conjured test", 3, 3))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("foo", app.items[0].name)
+        assertEquals(1, app.items[0].quality)
+        assertEquals(2, app.items[0].sellIn)
 
+    }
+
+    @Test
+    fun `should conjured item decrease quality by 4 after sellin`() {
+        val items = listOf(Item("Conjured test", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(6, app.items[0].quality)
+        assertEquals(-1, app.items[0].sellIn)
     }
 
 }
