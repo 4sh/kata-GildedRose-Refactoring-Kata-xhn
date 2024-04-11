c6bd77aaefde08d169e101fff07109ab10289a53
Deborah Pereira <deborah.pereira@4sh.fr>
2024-04-11 11:47:46 +0200

    add method to update quality

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,23 +7,23 @@ class GildedRose(var items: List<Item>) {
             if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0) {
                     if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+                        item.decrementQuality()
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.incrementQuality()
 
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
                             }
                         }
                     }
@@ -39,7 +39,7 @@ class GildedRose(var items: List<Item>) {
                     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
+                                item.decrementQuality()
                             }
                         }
                     } else {
@@ -47,11 +47,19 @@ class GildedRose(var items: List<Item>) {
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.incrementQuality()
                     }
                 }
             }
         }
+    }
+
+    private fun Item.incrementQuality() {
+        quality = quality + 1
+    }
+
+    private fun Item.decrementQuality() {
+        quality = quality - 1
     }
 
 }
