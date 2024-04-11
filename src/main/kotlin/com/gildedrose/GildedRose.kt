aa5b91b799ced588ae77ee842cacb237bf7d0eea
Xavier Hanin <xavier.hanin@4sh.fr>
2024-04-11 10:36:16 +0200

    get back to original gilded rose code source

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,48 +1,57 @@
 package com.gildedrose
 
-const val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
-const val SULFURAS = "Sulfuras, Hand of Ragnaros"
-const val BRIE = "Aged Brie"
-
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
-        items.forEach { item ->
-
-            if (item.name != SULFURAS) {
-                when {
-                    item.name == BACKSTAGE && item.sellIn < 6 -> {
-                        item.addQuality(3)
+        for (i in items.indices) {
+            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].quality > 0) {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].quality = items[i].quality - 1
                     }
-
-                    item.name == BACKSTAGE && item.sellIn < 11 -> {
-                        item.addQuality(2)
-                    }
-
-                    item.name in listOf(BACKSTAGE, BRIE) -> item.addQuality(1)
-                    else -> item.addQuality(-1)
                 }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
 
-                item.sellIn -= 1
-
-                if (item.sellIn < 0) {
-                    if (item.name == BRIE) {
-                        item.addQuality(1)
-                    } else {
-                        if (item.name != BACKSTAGE) {
-                            item.addQuality(-1)
-                        } else {
-                            item.addQuality(-item.quality)
+                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
                         }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].sellIn = items[i].sellIn - 1
+            }
+
+            if (items[i].sellIn < 0) {
+                if (items[i].name != "Aged Brie") {
+                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                items[i].quality = items[i].quality - 1
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
                     }
                 }
             }
         }
     }
 
-    private fun Item.addQuality(value: Int) {
-        quality += value
-        quality = quality.coerceIn(0, 50)
-    }
 }
-
--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -15,5 +15,3 @@ internal class GildedRoseTest {
     }
 
 }
-
-
