4b6b6ac61b9a21eb015c2c6d5e1fdc1d45aa6195
Mael CALECA <mael.caleca@4sh.fr>
2024-04-11 12:06:03 +0200

    Improving name of items and conditions

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,20 +1,24 @@
 package com.gildedrose
 
+private const val AGED_BRIE = "Aged Brie"
+private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+private const val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.decrementQuality()
+            when (item.name) {
+                AGED_BRIE -> {
+                    if (item.quality < 50) {
+                        item.incrementQuality()
                     }
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.incrementQuality()
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                BACKSTAGE_PASS -> {
+                    if (item.quality < 50) {
+                        item.incrementQuality()
+
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.incrementQuality()
@@ -28,17 +32,23 @@ class GildedRose(var items: List<Item>) {
                         }
                     }
                 }
+                SULFURAS -> {}
+                else -> {
+                    if (item.quality > 0) {
+                        item.decrementQuality()
+                    }
+                }
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
+            if (item.name != SULFURAS) {
                 item.sellIn--
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name != AGED_BRIE) {
+                    if (item.name != BACKSTAGE_PASS) {
                         if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            if (item.name != SULFURAS) {
                                 item.decrementQuality()
                             }
                         }
